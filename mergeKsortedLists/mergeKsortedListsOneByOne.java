8edf98df77cce69106e744748c1d5d1b6bc92c62
Neerav Bhaskarla <neeravbhaskarla@Neeravs-MacBook-Air.local>
2022-07-07 00:25:42 +0530

    Time Complexity: O(kN) k: Number of linked lists N: Nodes in final linked lists
    Space Complexity: O(1)

---
+++ mergeKsortedLists/mergeKsortedListsOneByOne.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int min_index = 0;
+        ListNode head = new ListNode(0); // For result (head.next)
+        ListNode h = head; // For Iteration
+        while(true){
+            boolean isBreak = true;
+            int min = Integer.MAX_VALUE;
+            for(int i=0; i<lists.length; i++){
+                if(lists[i]!=null){
+                    if(lists[i].val<min){
+                        min_index = i;
+                        min = lists[i].val;   
+                    }
+                    isBreak = false;
+                }
+            }
+            if(isBreak) break;
+            h.next = lists[min_index];
+            h = h.next;
+            lists[min_index] = lists[min_index].next;
+        }
+        return head.next;
+    }
