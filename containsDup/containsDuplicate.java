1cae6a27b55b28aa3541fb489f298f95371c512c
Neerav Bhaskarla <neeravbhaskarla@Neeravs-MacBook-Air.local>
2022-07-07 20:36:30 +0530

    contains duplicate number

---
+++ containsDup/containsDuplicate.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hash = new HashSet<>();
+        for(int i=0; i<nums.length; i++)
+            if(!hash.add(nums[i]))
+                return true;
+        return false;
+    }
