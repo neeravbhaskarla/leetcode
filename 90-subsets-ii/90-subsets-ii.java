b60d89b7a5355669af9748498c000ec8067f622a
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-08-20 03:10:08 +0530

    Time: 2 ms (86.33%), Space: 43.8 MB (74.07%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,30 +1,18 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        Set<String> seen = new HashSet<>();
-        List<List<Integer>> result = new ArrayList<>();
-        int numsLength = nums.length;
-        int maxNumOfSubsets = (int) Math.pow(2, numsLength);
-        
-        for(int subSetIndex = 0; subSetIndex<maxNumOfSubsets; subSetIndex++){
-            StringBuilder hashCode = new StringBuilder();
-            List<Integer> currentSubset = new ArrayList<>();
-            
-            for(int iter = 0; iter<numsLength; iter++){
-                int mask = 1 << iter;
-                int isSubset = mask & subSetIndex;
-                
-                if(isSubset!=0){
-                    currentSubset.add(nums[iter]);
-                    hashCode.append(nums[iter]).append(',');
-                }
-            }
-            if(!seen.contains(hashCode.toString())){
-                seen.add(hashCode.toString());
-                result.add(currentSubset);   
+        List<List<Integer>> subsets = new ArrayList<>();
+        subsets.add(new ArrayList<Integer>());
+        int subsetSize = 0;
+        for(int i=0; i<nums.length; i++){
+            int subsetIndex = (i>=1 && nums[i] == nums[i-1])?subsetSize:0;
+            subsetSize = subsets.size();
+            for(int j = subsetIndex; j<subsetSize; j++){
+                List<Integer> currentSubset = new ArrayList<>(subsets.get(j));
+                currentSubset.add(nums[i]);
+                subsets.add(currentSubset);
             }
         }
-        
-        return result;
+        return subsets;
     }
 }
