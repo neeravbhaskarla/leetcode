80677c96d0f9d907efbc41d72b1491585a2eb6ec
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-08-23 23:26:09 +0530

    Time: 7 ms (66.91%), Space: 97.3 MB (67.07%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -9,23 +9,36 @@
  * }
  */
 class Solution {
+    
+    public ListNode reverseLinkedList(ListNode node){
+        ListNode prev = null;
+        ListNode iter = node;
+        
+        while(iter!=null){
+            ListNode next = iter.next;
+            iter.next = prev;
+            prev = iter;
+            iter = next;
+        }
+        return prev;
+    }
     public boolean isPalindrome(ListNode head) {
-        List<Integer> vals = new ArrayList<>();
+        if(head == null) return true;
         
-        ListNode currentNode = head;
-        while(currentNode!=null){
-            vals.add(currentNode.val);
-            currentNode = currentNode.next;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast.next!=null && fast.next.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
         }
         
-        List<Integer> reversedVals = new ArrayList<>();
-        for(int i = vals.size() - 1; i>=0; i--){
-            reversedVals.add(vals.get(i));
-        }
+        ListNode p1 = head;
+        ListNode p2 = reverseLinkedList(slow.next);
         
-        for(int i=0; i<vals.size(); i++){
-            if(vals.get(i) != reversedVals.get(i))
-                return false;
+        while(p2!=null){
+            if(p1.val != p2.val) return false;
+            p1 = p1.next;
+            p2 = p2.next;
         }
         return true;
     }
