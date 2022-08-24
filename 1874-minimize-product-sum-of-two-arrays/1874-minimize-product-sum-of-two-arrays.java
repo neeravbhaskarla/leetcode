52bb02aec293170124e85aba6628382896fefeac
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-08-25 02:27:56 +0530

    Time: 5 ms (93.06%), Space: 93.3 MB (59.38%) - LeetHub

--- 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.java
+++ 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.java
@@ -1,13 +1,27 @@
 class Solution {
     public int minProductSum(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
+        int[] counter1 = new int[101];
+        int[] counter2 = new int[101];
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b)-> b-a);
+        for(int num: nums1) counter1[num]++;
+        for(int num: nums2) counter2[num]++;
         
-        for(int num: nums2) pq.add(num);
-        
+        int p1 = 0;
+        int p2 = 100;
         int productSum = 0;
-        for(int i = 0; i<nums1.length; i++) productSum+= nums1[i] * pq.poll();
+        
+        while(p1<101 && p2>0){
+            
+            while(p1<101 && counter1[p1] == 0) p1+=1;
+            while(p2>0 && counter2[p2] == 0) p2-=1;
+            
+            if(p1 == 101 || p2 == 0) break;
+            
+            int occ = Math.min(counter1[p1], counter2[p2]);
+            productSum += occ * p1 *p2;
+            counter1[p1]-=occ;
+            counter2[p2]-=occ;
+        }
         
         return productSum;
     }
