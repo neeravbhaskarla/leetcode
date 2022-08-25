d4fdd1f254eec50616aa0786617b26b93dabd56d
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-08-26 00:59:31 +0530

    Time: 4 ms (82.73%), Space: 119.2 MB (52.66%) - LeetHub

---
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
@@ -0,0 +1,19 @@
+class SparseVector {
+    public int[] nums;
+    SparseVector(int[] nums) {
+        this.nums = nums;
+    }
+    
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int result = 0;
+        int i=0;
+        for(int num: vec.nums) result += nums[i++]*num;
+        return result;
+    }
+}
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1 = new SparseVector(nums1);
+// SparseVector v2 = new SparseVector(nums2);
+// int ans = v1.dotProduct(v2);
