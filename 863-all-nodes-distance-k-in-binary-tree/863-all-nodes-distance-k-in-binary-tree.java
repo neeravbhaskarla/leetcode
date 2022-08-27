48f5c44695951593891074f299fd2a31deca0cd3
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-08-27 12:25:54 +0530

    Time: 21 ms (27.14%), Space: 43.4 MB (37.89%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private Map<TreeNode, TreeNode> parent = new HashMap<>();
+    
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        
+        dfs(root, null);
+        Set<TreeNode> seen = new HashSet<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(target);
+        seen.add(target);
+        
+        int dist = 0;
+        while(!queue.isEmpty()){
+            
+            if(dist == k){
+                return createListFromQueue(queue);
+            }
+            
+            int size = queue.size();
+            
+            for(int i=0; i<size; i++){
+                TreeNode node = queue.remove();
+                if(node.left!=null && !seen.contains(node.left)){
+                    queue.add(node.left);
+                    seen.add(node.left);
+                }
+                if(node.right!=null && !seen.contains(node.right)){
+                    queue.add(node.right);
+                    seen.add(node.right);
+                }   
+                TreeNode parentNode = parent.get(node);
+                if(parentNode!=null && !seen.contains(parentNode)){
+                    queue.add(parentNode);
+                    seen.add(parentNode);
+                }
+            }
+            dist++;
+        }
+        
+        return new ArrayList<>();
+    }
+    
+    public List<Integer> createListFromQueue(Queue<TreeNode> queue){
+        List<Integer> list = new ArrayList<>();
+        
+        while(queue.size()>0){
+            list.add(queue.remove().val);
+        }
+        
+        return list;
+    }
+    
+    private void dfs(TreeNode node, TreeNode parentNode){
+        if(node == null) return;
+        parent.put(node, parentNode);
+        dfs(node.left, node);
+        dfs(node.right, node);
+    }
+}
