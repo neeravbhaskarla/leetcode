44400eb0443e641a6ecec87a0e2726cf6a71d0de
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-08-27 22:06:30 +0530

    Time: 2 ms (90.77%), Space: 45.2 MB (7.36%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,19 +1,18 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int count = 0;
-        int[] dum_nums = Arrays.copyOfRange(nums, 0, nums.length);
-        Arrays.sort(dum_nums);
-        map.put(dum_nums[0], 0);
-        for(int i=1; i<dum_nums.length; i++){
-            if(dum_nums[i-1]==dum_nums[i]){
-                map.put(dum_nums[i], count);
-                continue;
-            }
-            count = i;
-            map.put(dum_nums[i], i);
+        int[] count = new int[101];
+        int[] res = new int[nums.length];
+        
+        for(int i=0; i<nums.length; i++){
+            count[nums[i]]++;
         }
-        for(int i=0; i<nums.length; i++) nums[i] = map.get(nums[i]);
+        for(int i=1; i<101; i++){
+            count[i]+=count[i-1];
+        }
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 0) nums[i] = 0;
+            else nums[i] = count[nums[i]-1];
+        }
         return nums;
     }
 }
