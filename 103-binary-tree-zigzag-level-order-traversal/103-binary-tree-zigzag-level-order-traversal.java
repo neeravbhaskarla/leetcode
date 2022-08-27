01aa51cc8f92f550b9d17389002e7ad483771b7c
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-08-27 10:54:21 +0530

    Time: 1 ms (96.37%), Space: 42.4 MB (76.19%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -4,44 +4,27 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-    if (root == null) {
-      return new ArrayList<List<Integer>>();
+    List<List<Integer>> result = new ArrayList<>();
+    public void levelOrderTraversal(TreeNode root, int level){
+        if(root == null) return;
+        if(level == result.size()) result.add(new ArrayList<>());
+        if((level+1)%2==0) result.get(level).add(0, root.val);
+        else result.get(level).add(root.val);
+        levelOrderTraversal(root.left, level+1);
+        levelOrderTraversal(root.right, level+1);
     }
-
-    List<List<Integer>> results = new ArrayList<List<Integer>>();
-    LinkedList<TreeNode> node_queue = new LinkedList<TreeNode>();
-    node_queue.addLast(root);
-    node_queue.addLast(null);
-
-    LinkedList<Integer> level_list = new LinkedList<Integer>();
-    boolean is_order_left = true;
-
-    while (node_queue.size() > 0) {
-      TreeNode curr_node = node_queue.pollFirst();
-      if (curr_node != null) {
-        if (is_order_left)
-          level_list.addLast(curr_node.val);
-        else
-          level_list.addFirst(curr_node.val);
-
-        if (curr_node.left != null)
-          node_queue.addLast(curr_node.left);
-        if (curr_node.right != null)
-          node_queue.addLast(curr_node.right);
-
-      } else {
-        results.add(level_list);
-        level_list = new LinkedList<Integer>();
-        if (node_queue.size() > 0)
-          node_queue.addLast(null);
-        is_order_left = !is_order_left;
-      }
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        levelOrderTraversal(root, 0);
+        return result;
     }
-    return results;
-  }
 }
