722f0eac6d7046a16d9179a7e5561e393cafac5e
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-08-30 19:27:12 +0530

    Time: 6 ms (85.27%), Space: 42.4 MB (97.00%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,14 +1,6 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> hash = Map.of(
-            'M', 1000,
-            'D', 500,
-            'C', 100,
-            'L', 50,
-            'X', 10,
-            'V', 5,
-            'I', 1
-        );
+        Map<Character, Integer> hash = Map.of('M', 1000,'D', 500,'C', 100,'L', 50,'X', 10,'V', 5,'I', 1);
         int right = s.length()-1;
         int max_value = Integer.MIN_VALUE;
         int result =0;
