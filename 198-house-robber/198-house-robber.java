10b9f0319178b644ff70c4d79960f7ec1526770a
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-09-02 02:20:35 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (14.51%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -4,7 +4,8 @@ class Solution {
         int[] result = new int[n+1];
         result[n] = 0;
         result[n-1] = nums[n-1];
-        for(int i=n-2; i>=0; i--) result[i] = Math.max(result[i+1], result[i+2]+nums[i]);
+        for(int i=n-2; i>=0; i--) 
+            result[i] = Math.max(result[i+1], result[i+2]+nums[i]);
         return result[0];
     }
 }
