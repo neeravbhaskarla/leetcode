feb4e2287e8a1e0b2954cf6943d997411672c816
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-09-04 13:35:29 +0530

    Time: 2 ms (38.90%), Space: 42.9 MB (81.27%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -4,9 +4,8 @@ class Solution {
         subsets.add(new ArrayList<Integer>());
         int subsetSize = 0;
         for(int i=0; i<nums.length; i++){
-            int subsetIndex = (i>=1 && nums[i] == nums[i-1])?subsetSize:0;
             subsetSize = subsets.size();
-            for(int j = subsetIndex; j<subsetSize; j++){
+            for(int j = 0; j<subsetSize; j++){
                 List<Integer> currentSubset = new ArrayList<>(subsets.get(j));
                 currentSubset.add(nums[i]);
                 subsets.add(currentSubset);
