a60e901bdc4cdd3e6627c55518928b4174614e36
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-09-09 10:06:23 +0530

    Time: 10 ms (93.41%), Space: 130.5 MB (57.64%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -1,14 +1,34 @@
 class Solution {
     public int numberOfWeakCharacters(int[][] properties) {
-        Arrays.sort(properties, (a, b) -> (a[0] == b[0]) ? (b[1] - a[1]) : a[0] - b[0]);
-        int weakCharacter = 0;
-        int maxDefense = 0;
-        for(int i = properties.length - 1; i>=0; i--){
-            if(properties[i][1] < maxDefense){
-                weakCharacter++;
-            }
-            maxDefense = Math.max(maxDefense, properties[i][1]);
+        int maxAttack = 0;
+        
+        for (int[] property : properties) {
+            int attack = property[0];
+            maxAttack = Math.max(maxAttack, attack);
         }
-        return weakCharacter;
+        
+        int maxDefense[] = new int[maxAttack + 2];
+        for (int[] property : properties) {
+            int attack = property[0];
+            int defense = property[1];
+            
+            maxDefense[attack] = Math.max(maxDefense[attack], defense);
+        }
+
+        for (int i = maxAttack - 1; i >= 0; i--) {
+            maxDefense[i] = Math.max(maxDefense[i], maxDefense[i + 1]);
+        }
+        
+        int weakCharacters = 0;
+        for (int[] property : properties) {
+            int attack = property[0];
+            int defense = property[1];
+            
+            if (defense < maxDefense[attack + 1]) {
+                weakCharacters++;
+            }
+        }
+        
+        return weakCharacters;
     }
 }
