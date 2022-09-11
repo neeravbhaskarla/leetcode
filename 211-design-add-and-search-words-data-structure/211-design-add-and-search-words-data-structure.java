c42f0404f0acc7da5ff055b56bf6f897de7f72ed
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-09-11 23:01:30 +0530

    Time: 1685 ms (25.45%), Space: 311 MB (16.38%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,45 @@
+class WordDictionary {
+    WordDictionary[] children;
+    boolean isEnd;
+    public WordDictionary() {
+        children = new WordDictionary[26];
+        isEnd = false;
+    }
+    
+    public void addWord(String word) {
+        WordDictionary pCrawl = this;
+        for(int level = 0; level<word.length(); level++){
+            int index = word.charAt(level) - 'a';
+            if(pCrawl.children[index]==null) 
+                pCrawl.children[index] = new WordDictionary();
+            pCrawl = pCrawl.children[index];
+        }
+        pCrawl.isEnd = true;
+    }
+    
+    public boolean search(String word) {
+        WordDictionary pCrawl = this;
+        for(int i=0; i<word.length(); i++){
+            if(word.charAt(i) == '.'){
+                for(WordDictionary ch: pCrawl.children){
+                    if(ch!=null && ch.search(word.substring(i+1))){
+                        return true;
+                    }
+                }
+                return false;
+            }
+            int index = word.charAt(i) - 'a';
+            if(pCrawl.children[index]==null) 
+                return false;
+            pCrawl = pCrawl.children[index];
+        }
+        return pCrawl.isEnd;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
