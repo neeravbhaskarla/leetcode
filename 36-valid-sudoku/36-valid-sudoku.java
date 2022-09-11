bce100f39195e6b9ead146a9486b861dab742f31
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-09-11 18:29:23 +0530

    Time: 10 ms (42.67%), Space: 48.8 MB (12.27%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,12 +1,27 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> seen = new HashSet<>();
-        for(int r=0; r<9; r++){
-            for(int c=0; c<9; c++){
-                if(board[r][c] == '.') continue;
-                String s = "(" +board[r][c]+ ")";
-                if(!seen.add(r + s) || !seen.add(s + c) || !seen.add(r/3 + s + c/3))
-                    return false;
+        int N = 9;
+        Map<Integer, Set<Integer>> rows = new HashMap<>();
+        Map<Integer, Set<Integer>> cols = new HashMap<>();
+        Map<Integer, Set<Integer>> squares = new HashMap<>();
+        
+        for(int idx=0; idx<N; idx++){
+            rows.put(idx, new HashSet<Integer>());
+            cols.put(idx, new HashSet<Integer>());
+            squares.put(idx, new HashSet<Integer>());
+        }
+        for(int row = 0; row<N; row++){
+            for(int col = 0; col<N; col++){
+                if(board[row][col]!='.'){
+                    int val = board[row][col] - '0';
+                    if(rows.get(row).contains(val) ||
+                       cols.get(col).contains(val) ||
+                       squares.get((row/3) * 3+col/3).contains(val))
+                        return false;
+                    rows.get(row).add(val);
+                    cols.get(col).add(val);
+                    squares.get((row/3) * 3 +col/3).add(val);
+                }
             }
         }
         return true;
