7ac96fb3be3791ab7d8321fcf45c5fdc5c6d7e22
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-09-13 10:24:16 +0530

    Time: 2 ms (79.27%), Space: 47.9 MB (47.93%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.java
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -2,22 +2,22 @@ class Solution {
     public boolean validUtf8(int[] data) {
         int mask1 = 1<<7;
         int mask2 = 1<<6;
-        int numberOfBytes = 0;
+        int numOfDigits = 0;
         for(int i=0; i<data.length; i++){
-            if(numberOfBytes == 0){
-                if(data[i]>255) return false; 
-                if((data[i] & 128) == 0) {
-                    numberOfBytes = 1;
-                } else if((data[i] & 224) == 192) {
-                    numberOfBytes = 2;
-                } else if((data[i] & 240) == 224) {
-                    numberOfBytes = 3;
-                } else if((data[i] & 248) == 240) {
-                    numberOfBytes = 4;
-                } else {
+            if(numOfDigits == 0){
+                int mask = 1<<7;
+                while((mask&data[i])!=0){
+                    numOfDigits++;
+                    mask = mask>>1;
+                }
+                
+                if(numOfDigits == 0){
+                    continue;
+                }
+                
+                if(numOfDigits > 4 || numOfDigits == 1){
                     return false;
                 }
-                if(numberOfBytes == 0) continue;
             }
             
             else{
@@ -25,8 +25,8 @@ class Solution {
                     return false;
                 }
             }
-            numberOfBytes--;
+            numOfDigits--;
         }
-        return numberOfBytes == 0;
+        return numOfDigits == 0;
     }
 }
