010ac75fa54cece1d1555918c4a96bdf35cc98f9
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-09-14 20:33:25 +0530

    Time: 8 ms (99.71%), Space: 67.2 MB (99.71%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -1,26 +1,21 @@
 class Solution {
-    public int pseudoPalindromicPaths (TreeNode root) {
-        int count = 0, path = 0;
+    int count = 0;
         
-        Deque<Pair<TreeNode, Integer>> stack = new ArrayDeque();
-        stack.push(new Pair(root, 0));
-        while (!stack.isEmpty()) {
-            Pair<TreeNode, Integer> p = stack.pop();
-            TreeNode node = p.getKey();
-            path = p.getValue();
-
-            if (node != null) {
-                path = path ^ (1 << node.val);
-                if (node.left == null && node.right == null) {
-                    if ((path & (path - 1)) == 0) {
-                        ++count;
-                    }
-                } else {
-                    stack.push(new Pair(node.right, path));
-                    stack.push(new Pair(node.left, path));
+    public void preorder(TreeNode node, int path) {
+        if (node != null) {
+            path = path ^ (1 << node.val);
+            if (node.left == null && node.right == null) {
+                if ((path & (path - 1)) == 0) {
+                    ++count;
                 }
             }
+            preorder(node.left, path);
+            preorder(node.right, path) ;
         }
+    }
+
+    public int pseudoPalindromicPaths (TreeNode root) {
+        preorder(root, 0);
         return count;
     }
 }
