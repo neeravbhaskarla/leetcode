cc914ee2d278aeaf849c06fa9a01f6a7c0351713
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-09-16 13:45:00 +0530

    Time: 146 ms (26.81%), Space: 71.5 MB (18.25%) - LeetHub

--- 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.java
+++ 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.java
@@ -1,28 +1,19 @@
 class Solution {
     public int lengthOfLongestSubstringTwoDistinct(String s) {
         int n = s.length();
-        if(n<3) return n;
+        if(n < 3) return n;
         int left = 0, right = 0;
-        int[] charset = new int[58];
-        int maxLength = 0;
+        Map<Character, Integer> hashmap = new HashMap<>();
+        int maxLength = 2;
         while(right<n){
-            charset[s.charAt(right) - 'A']++;
-            while(!distinct(charset)){
-                charset[s.charAt(left) - 'A']--;
-                left++;
+            hashmap.put(s.charAt(right), right++);
+            if(hashmap.size() == 3){
+                int del_idx = Collections.min(hashmap.values());
+                hashmap.remove(s.charAt(del_idx));
+                left = del_idx + 1;
             }
-            maxLength = Math.max(maxLength, right - left + 1);
-            right++;
+            maxLength = Math.max(maxLength, right - left);
         }
         return maxLength;
-    }
-    
-    public boolean distinct(int[] charset){
-        int count = 0;
-        for(int i=0; i<58; i++){
-            if(charset[i]>0) count++;
-            if(count > 2) return false;
-        }
-        return true;
     }
 }
