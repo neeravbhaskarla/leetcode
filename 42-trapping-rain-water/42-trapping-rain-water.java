19aece56a84608a807736f64c473b19256190450
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-09-18 10:36:33 +0530

    Time: 1 ms (99.76%), Space: 48.7 MB (57.27%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -4,19 +4,18 @@ class Solution {
         int right_max = 0;
         int left = 0;
         int right = height.length - 1;
-        int nums = 0;
+        int water = 0;
         
         while(left<right){
             left_max = Math.max(left_max, height[left]);
             right_max = Math.max(right_max, height[right]);
             if(left_max<right_max){
-                nums+= Math.max(0, left_max - height[left++]);
+                water += Math.max(0, left_max - height[left++]);
             }
             else{
-                nums+= Math.max(0, right_max - height[right--]);
+                water += Math.max(0, right_max - height[right--]);
             }
         }
-        
-        return nums;
+        return water;
     }
 }
