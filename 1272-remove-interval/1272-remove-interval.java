69fbc0429596c857918b29afd1ad0466ca933e2a
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-09-22 21:05:01 +0530

    Time: 45 ms (9.57%), Space: 77.6 MB (13.48%) - LeetHub

---
+++ 1272-remove-interval/1272-remove-interval.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) {
+        List<List<Integer>> result = new ArrayList<>();
+        int start = toBeRemoved[0], end = toBeRemoved[1];
+        for(int[] interval: intervals){
+            if(interval[1]<=start || interval[0]>=end){
+                result.add(new ArrayList<>(Arrays.asList(interval[0], interval[1])));
+            }
+            else{
+                if(interval[0]<start) result.add(new ArrayList<>(Arrays.asList(interval[0], start)));
+                if(interval[1]>end) result.add(new ArrayList<>(Arrays.asList(end, interval[1])));
+            }
+        }
+        return result;
+    }
+}
