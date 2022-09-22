3008cc16f58e21d20db96c79632296fb6eb82b86
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-09-23 01:52:58 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (99.39%) - LeetHub

---
+++ 1180-count-substrings-with-only-one-distinct-letter/1180-count-substrings-with-only-one-distinct-letter.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countLetters(String s) {
+        int total = 0;
+        for(int left = 0, right = 0; right<=s.length(); right++){
+            if(right == s.length() || s.charAt(left)!=s.charAt(right)){
+                int lenSubstring = right - left;
+                total += (1+lenSubstring)*lenSubstring/2;
+                left = right;
+            }
+        }
+        return total;
+    }
+}
