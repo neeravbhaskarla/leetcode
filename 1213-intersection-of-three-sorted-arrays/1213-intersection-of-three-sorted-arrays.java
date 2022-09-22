0c76222af53b5adad4b5742da3fd3007fc3cb4c0
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-09-23 00:42:34 +0530

    Time: 7 ms (34.62%), Space: 46.1 MB (79.66%) - LeetHub

--- 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.java
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.java
@@ -1,20 +1,24 @@
 class Solution {
     public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        int n = arr1.length;
+        int iter1 = 0, iter2 = 0, iter3 = 0;
         List<Integer> result = new ArrayList<>();
-        Map<Integer, Integer> arrMap = new HashMap<>();
-        for(int num: arr1){
-            arrMap.put(num, arrMap.getOrDefault(num, 0)+1);
-        }
-        for(int num: arr2){
-            arrMap.put(num, arrMap.getOrDefault(num, 0)+1);
-        }
-        for(int num: arr3){
-            arrMap.put(num, arrMap.getOrDefault(num, 0)+1);
-        }
-            
-        for(int key: arrMap.keySet()){
-            if(arrMap.get(key) == 3)
-                result.add(key);
+        while(iter1<n && iter2<n && iter3<n){
+            if(arr1[iter1] == arr2[iter2] && arr2[iter2] == arr3[iter3]){
+                result.add(arr1[iter1]);
+                iter1++; iter2++; iter3++;
+            }
+            else{
+                if(arr1[iter1] < arr2[iter2]){
+                    iter1++;
+                }
+                else if(arr2[iter2] < arr3[iter3]){
+                    iter2++;
+                }
+                else{
+                    iter3++;
+                }
+            }
         }
         return result;
     }
