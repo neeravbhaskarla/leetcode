f305b931138f3b32bc40ade4873eeea2decde85b
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-09-24 21:15:50 +0530

    Time: 9 ms (11.97%), Space: 57.7 MB (66.29%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -1,21 +1,15 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int n = nums.length;
-        if (n == 0) {
-            return 0;
-        }
-        int left = 0;
-        int right = 0;
-        int sum = 0;
-        int minLen = Integer.MAX_VALUE;
-        while(right<n){
-            sum+=nums[right];
-            while(sum>=target){
-                minLen = Math.min(minLen, right+1-left);
-                sum-=nums[left++];
+        int minLength = 0;
+        int start = 0, end = 0, sum = 0;
+        while (end < nums.length) {
+            sum += nums[end];
+            while (sum >= target) {
+                minLength = minLength == 0 || minLength > (end - start + 1) ? (end - start + 1) : minLength;
+                sum -= nums[start++];
             }
-            right++;
+            end ++;
         }
-        return minLen == Integer.MAX_VALUE?0: minLen;
+        return minLength;
     }
 }
