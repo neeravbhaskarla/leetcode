1af0b3c6501f6694e0cb0588541c07e389190fa1
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-10-05 12:56:52 +0530

    Time: 1 ms (88.59%), Space: 45.9 MB (20.33%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.java
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.java
@@ -4,44 +4,33 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
-    public TreeNode addOneRow(TreeNode root, int val, int depth) {
-        if(depth == 1){
-            TreeNode n = new TreeNode(val);
-            n.left = root;
+public class Solution {
+    public TreeNode addOneRow(TreeNode t, int v, int d) {
+        if (d == 1) {
+            TreeNode n = new TreeNode(v);
+            n.left = t;
             return n;
         }
-        Queue<TreeNode> queue = new ArrayDeque();
-        queue.add(root);
-        int currDepth = 1;
-        while(currDepth<depth-1){
-            Queue<TreeNode> temp = new ArrayDeque();
-            while(!queue.isEmpty()){
-                TreeNode node = queue.remove();
-                if(node.left!=null) temp.add(node.left);
-                if(node.right!=null) temp.add(node.right);
-            }
-            queue = temp;
-            currDepth++;
-        }
-        while(!queue.isEmpty()){
-            TreeNode node = queue.remove();
-            TreeNode temp = node.left;
+        insert(v, t, 1, d);
+        return t;
+    }
+
+    public void insert(int val, TreeNode node, int depth, int n) {
+        if (node == null)
+            return;
+        if (depth == n - 1) {
+            TreeNode t = node.left;
             node.left = new TreeNode(val);
-            node.left.left = temp;
-            temp = node.right;
+            node.left.left = t;
+            t = node.right;
             node.right = new TreeNode(val);
-            node.right.right = temp;
+            node.right.right = t;
+        } else {
+            insert(val, node.left, depth + 1, n);
+            insert(val, node.right, depth + 1, n);
         }
-        return root;
     }
 }
