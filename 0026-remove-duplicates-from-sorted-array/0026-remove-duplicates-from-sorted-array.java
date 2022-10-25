470c940e76bd94ab575a5d8df86c3229fe266532
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-10-26 01:53:04 +0530

    Time: 1 ms (100.00%), Space: 43.7 MB (97.06%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,11 +1,14 @@
 class Solution {
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
     public int removeDuplicates(int[] nums) {
         int left = 0, right = 0;
         while(right<nums.length){
             if(nums[left]!=nums[right] && left<nums.length){
-                int temp = nums[++left];
-                nums[left] = nums[right];
-                nums[right] = temp;
+                swap(nums, ++left, right);
             }
             right++;
         }
