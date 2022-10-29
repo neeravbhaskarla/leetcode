a5b5aa86f8f2c19aa5681383fa680788c968087a
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2022-10-29 13:08:41 +0530

    Time: 76 ms (94.52%), Space: 56.3 MB (93.61%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.java
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.java
@@ -6,13 +6,13 @@ class Solution {
             indices.add(i);
         }
         Collections.sort(indices, Comparator.comparingInt(i -> -growTime[i]));
-        int max = 0;
-        for (int i = 0, plantSum = 0; i < n; ++i) {
+        int result = 0;
+        for (int i = 0, curPlantTime = 0; i < n; ++i) {
             int idx = indices.get(i);
-            int time = plantSum + plantTime[idx] + growTime[idx];
-            max = Math.max(max, time);
-            plantSum += plantTime[idx];
+            int time = curPlantTime + plantTime[idx] + growTime[idx];
+            result = Math.max(result, time);
+            curPlantTime += plantTime[idx];
         }
-        return max;
+        return result;
     }
 }
